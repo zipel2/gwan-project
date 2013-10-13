ef0c1f32491a8af3e583f5681dfa9d35abbf5624
JaeGwan <zipel2@naver.com>
2013-10-13 19:23:51 +0900

    first week

---
+++ src/main/java/org/nhnnext/web/BoardController.java
@@ -0,0 +1,20 @@
+package org.nhnnext.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BoardController {
+    @RequestMapping("/board/form")
+    public String form() {
+        return "form";
+        
+        
+}
+    @RequestMapping(value="/board", method=RequestMethod.POST)
+    public String create(String title, String contents) { 
+    	System.out.println("title : " + title + " contents : " + contents); 
+    	return "redirect:/";
+    }    
+}
